e102b19ae08f61fedd46a5f8763f8ac4e6adc195
Hurukawa2121 <shujifurukawa1213@gmail.com>
2025-01-04 23:10:05 +0900

    child.cリファクタ

--- src/child.c
+++ src/child.c
@@ -12,50 +12,66 @@
 #include "container.h"
 #include "userns.h"
 
+
+bool set_config(struct child_config *config) {
+    if (sethostname(config->hostname, strlen(config->hostname)) < 0) {
+        perror("sethostname failed");
+        return false;
+    }
+
+    if (mounts(config) < 0) {
+        fprintf(stderr, "mounts failed\n");
+        return false;
+    }
+
+    if (userns(config) < 0) {
+        fprintf(stderr, "userns failed\n");
+        return false;
+    }
+    return true;
+}
+
+bool switch_uid_gid(int uid, int gid) {
+    if (setgroups(1, (gid_t[]){uid}) ||
+        setresgid(uid, uid, uid) ||
+        setresuid(uid, uid, uid)) {
+        perror("failed setresuid/setresgid");
+        return false;
+    }
+
+    if (capabilities() < 0) {
+        fprintf(stderr, "capabilities() failed\n");
+        return false;
+    }
+    if (syscalls() < 0) {
+        fprintf(stderr, "syscalls() failed\n");
+        return false;
+    }
+
+    if (close(config->fd) < 0) {
+        perror("close fd");
+        return false;
+    }
+    return true;
+}
+
 int child(void *arg) {
     struct child_config *config = (struct child_config*) arg;
+
     // ホスト名設定
-    if (sethostname(config->hostname, strlen(config->hostname)) != 0) {
-        perror("sethostname failed");
-        return -1;
-    }
-
-    if (mounts(config) != 0) {
-        fprintf(stderr, "mounts failed\n");
-        return -1;
-    }
-
-    if (userns(config) != 0) {
-        fprintf(stderr, "userns failed\n");
+    if (!set_config(config)) {
         return -1;
     }
 
     // userns 内で uid/gidを切り替え
-    fprintf(stderr, "=> switching to uid %d / gid %d...\n", config->uid, config->uid);
-    if (setgroups(1, (gid_t[]){config->uid}) ||
-        setresgid(config->uid, config->uid, config->uid) ||
-        setresuid(config->uid, config->uid, config->uid)) {
-        perror("failed setresuid/setresgid");
-        return -1;
-    }
-
-    if (capabilities() != 0) {
-        fprintf(stderr, "capabilities() failed\n");
-        return -1;
-    }
-    if (syscalls() != 0) {
-        fprintf(stderr, "syscalls() failed\n");
-        return -1;
-    }
-
-    if (close(config->fd) != 0) {
-        perror("close fd");
+    ffprintf(stderr, "=> switching to uid %d / gid %d...\n", uid, uid);
+    if (!switch_uid_gid(config->uid, config->uid)) {
         return -1;
     }
 
     // 実行
     fprintf(stderr, "=> execve(%s)...\n", config->argv[0]);
-    if (execve(config->argv[0], config->argv, NULL) != 0) {
+    if (execve(config->argv[0], config->argv, NULL) < 0) {
         perror("execve failed");
         return -1;
     }
